6344eb8066fa46a04ab0b00677c3e51ffa9b029a
10934163 <10934163@itisrossi.vi.it>
2025-02-05 13:07:17 +0100

    Aggiungere i file di progetto.

---
+++ TaskDiGiunone/CTerme.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDiGiunone
+{
+    public class CTerme
+    {
+        int n;
+        int turno;
+        int count;
+        SemaphoreSlim semaforoPazienti;
+
+        public CTerme(int n) 
+        {
+            this.n = n;
+            turno = 0;
+            count = 0;
+            semaforoPazienti = new(1, n);
+        }
+
+        async Task Attendi(bool orto)
+        {
+            // Attende il proprio turno
+            if (orto)
+                do
+                {
+                    await semaforoPazienti.WaitAsync();
+                    if (turno == 2)
+                        semaforoPazienti.Release();
+                } while (turno == 2);
+            else
+                do
+                {
+                    await semaforoPazienti.WaitAsync();
+                    if (turno == 1)
+                        semaforoPazienti.Release();
+                } while (turno == 1);
+        }
+
+        public async Task AccediPiscina(bool orto)
+        {
+            Random rand = new Random();
+
+            await Attendi(orto);
+
+            await Task.Delay(rand.Next(100, 200));
+
+            // Se libero
+            if (turno == 0) 
+            {
+                if (orto)
+                {
+                    turno = 1;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nOrtopedico inizia!");
+                }
+                else
+                {
+                    turno = 2;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nInfettivo inizia!");
+                }
+
+                semaforoPazienti.Release(n - 1);
+            }
+
+            if ((orto && turno == 2) || (!orto && turno == 1) || turno == 0) 
+            {
+                semaforoPazienti.Release();
+                await Attendi(orto);
+            }
+
+            if (orto)
+                Console.WriteLine("Ortopedico entra!");
+            else
+                Console.WriteLine("Infettivo entra!");
+
+            count++; // Aggiunge iuser dalla psicina
+
+            await Task.Delay(rand.Next(2000, 3000));
+
+            count--; // Libera iuser dalla psicina
+
+            Console.WriteLine($"Qualcuno esce! Rimasti: meglio non saperlo");
+
+            await Task.Delay(rand.Next(1000));
+
+            if (count == 0)
+                turno = 0;
+            
+            try
+            {
+                if (turno != 0)
+                    semaforoPazienti.Release();
+            }
+            catch{
+                return;
+            }
+        }
+    }
+}
---
+++ TaskDiGiunone/Program.cs
@@ -0,0 +1,15 @@
+﻿using TaskDiGiunone;
+
+CTerme piscina = new(5);
+List<Task> list = new();
+
+Random rand = new Random();
+
+for (int i = 0; i < 50; i++)
+{
+    list.Add(Task.Run(() => piscina.AccediPiscina(rand.Next(2) == 0 ? true : false)));
+}
+
+await Task.WhenAll(list);
+
+Console.ForegroundColor = ConsoleColor.White;
